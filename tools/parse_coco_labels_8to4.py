3dda1c1b96623099f12cdff2db43eeba0860f616
HRan2004 <42177278+HRan2004@users.noreply.github.com>
2021-04-18 21:10:51 +0800

    新增数据集反向计算工具

--- tools/parse_coco_labels_8to4.py
+++ tools/parse_coco_labels_8to4.py
@@ -1,4 +1,5 @@
 import os
+import numpy
 
 read_path = "E:\\coco128\\labels\\train2017\\"
 save_path = "E:\\coco128\\labels\\train2017\\"
@@ -12,13 +13,17 @@ for file in os.listdir(read_path):
                 f = open(save_path + file, "w", encoding='UTF-8')
                 newLines = []
                 for line in lines:
-                    at = line.split()
-                    at[1] = str(float(at[1])-float(at[3])/2)
-                    at[2] = str(float(at[2])-float(at[4])/2)
-                    at[3] = str(float(at[1])+float(at[3]))
-                    at[4] = str(float(at[2])+float(at[4]))
+                    at = numpy.array(list(map(int,line.split())))
+                    x_min = at[0::2].min()
+                    x_max = at[0::2].max()
+                    y_min = at[1::2].min()
+                    y_max = at[1::2].max()
+                    x = (x_min+x_max)/2
+                    y = (y_min+y_max)/2
+                    w = x_max-x_min
+                    h = y_max-y_min
                     if len(at)==5:
-                        newLines.append(at[0]+" "+at[1]+" "+at[2]+" "+at[3]+" "+at[2]+" "+at[3]+" "+at[4]+" "+at[1]+" "+at[4]+"\n")
+                        newLines.append(str(x)+" "+str(y)+" "+str(w)+" "+str(h)+"\n")
                 print(file)
                 print(newLines)
                 f.writelines(newLines)
